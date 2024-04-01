878828c8d4339c8cd8faf035d1a13e7203134bef
egormyronchuk <egormyronchuk@gmail.com>
2024-04-02 00:23:18 +0300

    finish

--- HomeTask7/src/main/java/org/example/Main.java
+++ HomeTask7/src/main/java/org/example/Main.java
@@ -8,11 +8,7 @@ import org.example.Pets.Dog;
 
 public class Main {
     public static void main(String[] args) {
-        Dog sharik = new Dog("Sharik");
-        Man man = new Man("Yaroslav" , "Kravech" , 19 );
-        Woman woman = new Woman("Olya" , "Kravech" , 19);
-        Family family = new Family(woman , man);
-        System.out.println(family.toString());
+
     }
 
 
